52ab0d0dbcc178e417b16a7091876fb0f96af674
Jenny <5704@holbertonstudents.com>
2022-11-08 04:39:02 -0800

    Added functions that print char/chars

---
+++ function_char.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+int print_char(va_list type)
+{
+    	_putchar(va_arg(ap, int));
+	return (1);
+}
+
+int print_str(va_list type)
+{
+	int i;
+
+	i = 0;
+	while (i < (va_arg(ap, char)[i]) != '\0')
+	{
+		_putchar((va_arg(ap, char)[i]));
+		i = i + 1;
+	}
+	return (i);
+}
+
+int print_percent(va_list type)
+{
+	_putchar('%');
+	return (1);
+}
