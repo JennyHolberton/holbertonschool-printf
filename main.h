b5a80429db2c6abaaa64d0c4a745627c2648b0e0
pallavi149 <5811@holbertonschoolstudents.com>
2022-11-07 17:38:55 -0800

    main.h

---
+++ main.h
@@ -0,0 +1,16 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+struct datatypes
+{
+    char is_c;
+    string is_s;
+    char is_percent;
+	/*long as_long;
+    float as_float;
+    double as_double;
+    void* as_ptr*/
+};
+typedef struct datatypes d_types;
+
+int _printf(const char *format, ...);
+endif /*_MAIN_H_*/
